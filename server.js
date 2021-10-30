637406a75aa78a7193ed15bcf7e00890a8c6d3b4
carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>
2021-10-30 11:46:52 -0500

    se instalo express y se creo el archivo server.js
    Signed-off-by: carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+const route = express.Router();
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// Conexion base de datos.
+
